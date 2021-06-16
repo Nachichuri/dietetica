1f6db7a62d3f6ca8b03d28cf6d8ed46ba71cf95b
Nachichuri <nachichuri@gmail.com>
2021-06-16 19:39:09 -0300

    Added placeholders to templates

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 from django.db.models.deletion import SET_NULL
 
 class HomeInfo(models.Model):
@@ -17,7 +18,7 @@ class HomeInfo(models.Model):
     hours_oneline = models.CharField(max_length=100)
     hours_weekdays_morning = models.CharField(max_length=50)
     hours_weekdays_evening = models.CharField(max_length=50)
-    hours_weekdays = models.CharField(max_length=50)
+    hours_weekends = models.CharField(max_length=50)
 
     def __str__(self):
         return self.store_name
@@ -28,9 +29,11 @@ class ContactInfo(models.Model):
     phone = models.CharField(max_length=50)
     mobile = models.CharField(max_length=50, null=True, blank=True)
     email = models.CharField(max_length=100)
-    location = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
     facebook_page = models.CharField(max_length=100, null=True, blank=True)
     instagram_page = models.CharField(max_length=100, null=True, blank=True)
+    whatsapp_link = models.CharField(max_length=100, null=True, blank=True)
+    googlemaps_link = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.store_name
@@ -38,6 +41,7 @@ class ContactInfo(models.Model):
 
 class MainCategories(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField()
     image = models.ImageField(upload_to='main/images')
     description = models.TextField()
 
@@ -47,6 +51,7 @@ class MainCategories(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField()
     category = models.ForeignKey(MainCategories, on_delete=SET_NULL, null=True)
     image = models.ImageField(upload_to='main/images')
     description = models.TextField()
